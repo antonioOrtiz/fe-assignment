c3c48a35779eedd10f12f7bf205e730c3cb34596
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2020-07-17 12:30:01 -0400

    initial commit

---
+++ components/AppliancePackage.jsx
@@ -0,0 +1,14 @@
+import { useState, useEffect } from 'react';
+
+export default function AppliancePackage() {
+  return (
+    <>
+      <main
+        className="container
+      "
+      >
+        <h1>AppliancePackages</h1>
+      </main>
+    </>
+  );
+}
---
+++ pages/index.jsx
@@ -0,0 +1,11 @@
+import AppliancePackage from '../components/AppliancePackage.jsx';
+
+function HomePage() {
+  return (
+    <>
+      <AppliancePackage />
+    </>
+  );
+}
+
+export default HomePage;
