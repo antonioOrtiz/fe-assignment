53a294159c779104e55d957c36eb7a0ba3993fd3
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2020-07-19 17:28:30 -0400

    pulled in data from API endpoint

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,32 @@
 import AppliancePackage from '../components/AppliancePackage.jsx';
-
-function HomePage() {
+import axios from 'axios';
+function HomePage(data) {
+  console.log('data in homepage', data);
   return (
     <>
-      <AppliancePackage />
+      <AppliancePackage appliances={data} />
     </>
   );
 }
 
 export default HomePage;
+
+// This function gets called at build time on server-side.
+// It won't be called on client-side, so you can even do
+// direct database queries. See the "Technical details" section.
+export async function getStaticProps() {
+  // Call an external API endpoint to get posts.
+  // You can use any data fetching library
+
+  var res = await axios.get('https://demo3211013.mockable.io/ajmad');
+  var res = JSON.parse(JSON.stringify(res.data));
+  console.log('res ', res);
+
+  // By returning { props: posts }, the Blog component
+  // will receive `posts` as a prop at build time
+  return {
+    props: {
+      data: res,
+    },
+  };
+}
