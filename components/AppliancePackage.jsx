bab9f70c267b590060bfbaece2d419066baa4acf
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2020-07-19 17:26:37 -0400

    pulling in JSON to create appliance data

--- components/AppliancePackage.jsx
+++ components/AppliancePackage.jsx
@@ -1,14 +1,29 @@
 import { useState, useEffect } from 'react';
+import ApplianceCard from './AppilanceCard.jsx';
 
-export default function AppliancePackage() {
+export default function AppliancePackage({ appliances }) {
+  console.log('data in AppliancePackage', appliances.data);
   return (
     <>
-      <main
+      <div
         className="container
       "
       >
         <h1>AppliancePackages</h1>
-      </main>
+        <p>
+          Looking for a great deal on home appliances? Packages are the best
+          bet. Most appliance packages consist of a range, refrigerator,
+          over-the-range microwave, and dishwasher. However, some upscale brands
+          have packages that incorporate wall ovens, cooktops, and integrated
+          refrigerators.
+        </p>
+
+        <div className="cards">
+          {appliances.data.map(function (props) {
+            return <ApplianceCard {...props} />;
+          })}
+        </div>
+      </div>
     </>
   );
 }
