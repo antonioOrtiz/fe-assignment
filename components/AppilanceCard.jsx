e5bf9c912338fed86ba5d2d1464e76fd4777ec67
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2020-07-20 16:09:34 -0400

    fixed some layout issues, organized styles with postcss-sorting

--- components/AppilanceCard.jsx
+++ components/AppilanceCard.jsx
@@ -25,17 +25,36 @@ export default function ApplianceCard({
   return (
     <>
       <div className="card">
-        <div className="tile">
-          <img src={`https://assets.ajmadison.com/${folder}/${filename}.jpg`} />
+        <div className="grid-helper">
+          <div className="tile">
+            <img
+              className="product__image"
+              src={`https://assets.ajmadison.com/${folder}/${filename}.jpg`}
+            />
+          </div>
+          <div>
+            <h3 className="product__title">{brand}</h3>
+            <div>
+              <p className="product__description">{description}</p>
+              <img
+                className="quickShip"
+                src="/images/quickship-pdp.png"
+                alt="QuickShip"
+              />
+            </div>
+            <span className="product__final-price">{` $${final}`}</span> <br />
+            <span className="product__discount">
+              {`Save $${(Math.round((list_price - final) * 100) / 100).toFixed(
+                2
+              )} `}{' '}
+            </span>
+            <strike className="product_list-price">{`$${list_price}`}</strike>
+            <br />
+            <button className="product__viewpackage-button">
+              VIEW PACKAGE
+            </button>
+          </div>
         </div>
-        <h3>{brand}</h3>
-        <h4>{title}</h4>
-        <p>{description}</p>
-        <span>{`final price: $${final}`}</span> <br />
-        <span className="savings">{`Save $${list_price - final} `} </span>
-        <strike>{`$${list_price}`}</strike>
-        <br />
-        <button>VIEW PACKAGE</button>
       </div>
     </>
   );
--- components/AppliancePackage.jsx
+++ components/AppliancePackage.jsx
@@ -3,27 +3,31 @@ import ApplianceCard from './AppilanceCard.jsx';
 
 export default function AppliancePackage({ appliances }) {
   console.log('data in AppliancePackage', appliances.data);
+
   return (
     <>
+      {' '}
       <div
         className="container
-      "
+"
       >
-        <h1>AppliancePackages</h1>
+        {' '}
+        <h1>AppliancePackages</h1>{' '}
         <p>
+          {' '}
           Looking for a great deal on home appliances? Packages are the best
           bet. Most appliance packages consist of a range, refrigerator,
           over-the-range microwave, and dishwasher. However, some upscale brands
           have packages that incorporate wall ovens, cooktops, and integrated
-          refrigerators.
-        </p>
-
+          refrigerators.{' '}
+        </p>{' '}
         <div className="cards">
+          {' '}
           {appliances.data.map(function (props) {
             return <ApplianceCard {...props} />;
           })}
-        </div>
-      </div>
+        </div>{' '}
+      </div>{' '}
     </>
   );
 }
